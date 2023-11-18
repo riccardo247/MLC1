592b2bc8ba91a1e2903b722eb69cb313767da2eb
RR <40582352+riccardo247@users.noreply.github.com>
2023-11-18 17:04:31 +0000

    Update train_models.sh
    add few libraries

--- train_models.sh
+++ train_models.sh
@@ -19,14 +19,20 @@ git checkout v4.35.0
 
 # Install the Transformers package
 pip install git+https://github.com/huggingface/transformers.git@v4.35.0
+pip install torch
+pip install scikit-learn
+pip install matplotlib
 
 cd /MLC1/
 # Run Python scripts 
-python train_decoder.py --nepochs 50 --nlayers_decoder 1 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_1layer_alg.pt
-python train_decoder.py --nepochs 50 --nlayers_decoder 2 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_2layer_alg.pt
-python train_decoder.py --nepochs 50 --nlayers_decoder 3 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_3layer_alg.pt
-python train_decoder.py --nepochs 50 --nlayers_decoder 4 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_4layer_alg.pt
-python train_decoder.py --nepochs 50 --nlayers_decoder 4 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_5layer_alg.pt
+python train_decoder.py --nepochs 50 --nlayers_decoder 1 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_1layer_alg.pt >/log1.txt 2>&1 &
+python train_decoder.py --nepochs 50 --nlayers_decoder 2 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_2layer_alg.pt >/log2.txt 2>&1 &
+wait
+python train_decoder.py --nepochs 50 --nlayers_decoder 3 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_3layer_alg.pt >/log3.txt 2>&1 &
+python train_decoder.py --nepochs 50 --nlayers_decoder 4 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_4layer_alg.pt >/log4.txt 2>&1 &
+wait
+python train_decoder.py --nepochs 50 --nlayers_decoder 4 --batch_size 25 --episode_type algebraic --fn_out_model net-LLAMA_50ep_5layer_alg.pt >/log5.txt 2>&1 &
+wait
 python eval_llama.py --episode_type algebraic --sample_html --fn_out_model net-LLAMA_50ep_1layer_alg.pt >eval/net-LLAMA_50ep_1layer_alg.txt
 python eval_llama.py --episode_type algebraic --sample_html --fn_out_model net-LLAMA_50ep_2layer_alg.pt >eval/net-LLAMA_50ep_2layer_alg.txt
 python eval_llama.py --episode_type algebraic --sample_html --fn_out_model net-LLAMA_50ep_3layer_alg.pt >eval/net-LLAMA_50ep_3layer_alg.txt
